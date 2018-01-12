ceec0ffd24683cb27144a07d39b498fac8707981
yeyulin <yeyulin1216@163.com>
2018-01-12 15:38:59 +0800

    this is first commit.

---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="helloyy">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to YYL Vue demo! hh'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+a {
+  color: #42b983;
+}
+</style>
