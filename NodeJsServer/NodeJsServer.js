9a4c738a3f74fa9fb85ca2549502327284888d8d
yeyulin <yeyulin1216@163.com>
2018-01-13 15:23:35 +0800

    初步可以接收get参数。

--- NodeJsServer/NodeJsServer.js
+++ NodeJsServer/NodeJsServer.js
@@ -1,15 +1,25 @@
 //引入http模块
 var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+
 //设置主机名
 var hostName = '127.0.0.1';
 //设置端口
 var port = 9090;
 //创建服务
 var server = http.createServer(function(req,res){
+    var pathname = url.parse(req.url).pathname;
+    var paramStr = url.parse(req.url).query;
+    var param = querystring.parse(paramStr);
+
+    console.log(pathname);
+    console.log(param);
+
     res.setHeader('Content-Type','text/plain');
     res.end('hello nodejs');
-
 });
+
 server.listen(port,hostName,function(){
     console.log('服务器运行在%s:%d', hostName, port);
 });
